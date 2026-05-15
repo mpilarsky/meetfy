2f50e614b9a227d7b569b81e74caa1410f65475e
Maciej Pilarski <mpilarski8@gmail.com>
2026-05-15 22:53:54 +0200

    temporary routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import LoginPage from "./pages/LoginPage";
+import { Routes, Route } from "react-router-dom";
+
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PreferencesCreator from "./pages/PreferencesCreator";
 import ContactPage from "./pages/ContactPage";
@@ -9,10 +11,25 @@ import MyEventsPage from "./pages/MyEventsPage";
 import SearchPage from "./pages/SearchPage";
 import MyAccountPage from "./pages/MyAccountPage";
 import CreateEventPage from "./pages/CreateEventPage";
-import EventDetailsModal from "./components/EventDetailsModal";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/preferences" element={<PreferencesCreator />} />
+      <Route path="/contact" element={<ContactPage />} />
+
+      <Route path="/dashboard" element={<UserDashboard />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="/events" element={<MyEventsPage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/account" element={<MyAccountPage />} />
+      <Route path="/create-event" element={<CreateEventPage />} />
+    </Routes>
+  );
 }
 
 export default App;
